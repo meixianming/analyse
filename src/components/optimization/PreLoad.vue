b4936ddb8cd6a998ef4b5cf9700e13d9ecea18ba
Mei Xianming <meixm@jxgtmbjt.com>
2019-02-22 16:08:25 +0800

    图片懒加载/预加载

--- src/components/optimization/PreLoad.vue
+++ src/components/optimization/PreLoad.vue
@@ -4,6 +4,12 @@
       图片预加载就是在网页全部加载之前，提前加载图片。当用户需要查看时可直接从本地缓存中渲染，以提供给用户更好的体验，减少等待的时间。
       这种做法实际上牺牲了服务器的性能换取了更好的用户体验。
     </p>
+    <p>方法一：用CSS和JavaScript实现预加载。我们便可通过CSS的background属性将图片预加载到屏幕外的背景上。只要这些图片的路径保持不变，当它们在web页面的其他地方被调用时，浏览器就会在渲染过程中使用预加载（缓存）的图片</p>
+    <p>方法二：仅使用JavaScript实现预加载
+      <el-button type="primary"
+                 @click="preloader2">预加载二</el-button>
+    </p>
+    <p>方法三：使用ajax。即请求后台接口</p>
   </div>
 </template>
 <script>
@@ -11,6 +17,25 @@
     name: "PreLoad",
     data() {
       return {};
+    },
+    methods: {
+      preloader2() {
+        // 时间应该在mounted生命周期，当页面内容已挂靠完成后执行
+        if (document.images) {
+          var img1 = new Image();
+          var img2 = new Image();
+          var img3 = new Image();
+          img1.src = "http://domain.tld/path/to/image-001.gif";
+          img2.src = "http://domain.tld/path/to/image-002.gif";
+          img3.src = "http://domain.tld/path/to/image-003.gif";
+        }
+      },
+      preloader3() {
+        // 向后台发送接口直接请求数据
+        // 	$(function(){
+        //     $.get('图片的路径');
+        // })
+      }
     }
   };
 </script>
--- src/components/optimization/lazyLoad.vue
+++ src/components/optimization/lazyLoad.vue
@@ -1,5 +1,16 @@
+<style>
+  .lazy-load {
+    height: 100vh;
+    overflow: scroll;
+  }
+  .lazy-img {
+    min-height: 600px;
+  }
+</style>
 <template>
-  <div>
+  <div class="lazy-load"
+       ref="lazyLoad"
+       @scroll="lazyLoad">
     <p ref="a">页面中的img元素，如果没有src属性，浏览器就不会发出请求去下载图片，只有通过javascript设置了图片路径，浏览器才会发送请求。
       懒加载的原理就是先在页面中把所有的图片统一使用一张占位图进行占位，把正真的路径存在元素的“data-url”（这个名字起个自己认识好记的就行）属性里，要用的时候就取出来，再设置。</p>
     <p>延迟加载图片或符合某些条件时才加载某些图片。这样做的好处是减少不必要的访问数据库或延迟访问数据库的次数，因为每次访问数据库都是比较耗时的即只有真正使用该对象的数据时才会创建。懒加载的主要目的是作为服务器前端的优化，减少请求数或延迟请求数。</p>
@@ -7,7 +18,8 @@
       <li v-for="(item,index) in list"
           v-bind:key="index">
         <img alt="loading"
-             :src="item" />
+             class="lazy-img"
+             :data-url="item" />
       </li>
     </ul>
   </div>
@@ -18,18 +30,52 @@
     data() {
       return {
         list: [
-          "https://www.baidu.com/img/bd_logo.png",
-          "https://www.baidu.com/img/bd_logo.png",
-          "https://www.baidu.com/img/bd_logo.png",
-          "https://www.baidu.com/img/bd_logo.png",
-          "https://www.baidu.com/img/bd_logo.png",
-          "https://www.baidu.com/img/bd_logo.png",
-          "https://www.baidu.com/img/bd_logo.png",
-          "https://www.baidu.com/img/bd_logo.png",
-          "https://www.baidu.com/img/bd_logo.png"
-        ]
+          "http://axuebin.com/lazyload/img/img1.png",
+          "http://axuebin.com/lazyload/img/img2.png",
+          "http://axuebin.com/lazyload/img/img3.png",
+          "http://axuebin.com/lazyload/img/img4.png",
+          "http://axuebin.com/lazyload/img/img5.png",
+          "http://axuebin.com/lazyload/img/img6.png",
+          "http://axuebin.com/lazyload/img/img7.png",
+          "http://axuebin.com/lazyload/img/img8.png"
+        ],
+        timer: null,
+        imgList: "",
+        oldScrollTop: 0,
+        len: 0 // 设置len以便已加载过的图片就不重复加载了
       };
     },
-    mounted() {}
+    mounted() {
+      this.imgList = document.getElementsByClassName("lazy-img");
+    },
+    methods: {
+      debounce(fn) {
+        clearTimeout(this.timer);
+        this.timer = setTimeout(() => {
+          fn();
+        }, 200);
+      },
+      loadImg() {
+        const clientHeight = window.innerHeight;
+        for (let i = this.len; i < this.imgList.length; i++) {
+          const bound = this.imgList[i].getBoundingClientRect();
+          if (bound.top <= clientHeight + 100) {
+            this.imgList[i].src = this.imgList[i].getAttribute("data-url");
+          }
+        }
+      },
+      lazyLoad() {
+        // 如果下拉距离大于500px则自动加载
+        if (this.$refs.lazyLoad.scrollTop - this.oldScrollTop > 500) {
+          this.loadImg();
+          this.oldScrollTop = this.$refs.lazyLoad.scrollTop;
+        } else if (this.$refs.lazyLoad.scrollTop - this.oldScrollTop < 0) {
+          // 上拉不做处理
+          return;
+        } else {
+          this.debounce(this.loadImg);
+        }
+      }
+    }
   };
 </script>
